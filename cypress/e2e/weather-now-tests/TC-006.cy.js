6453a5fb44328020c277ffa393072cce1db49b3f
CaioBonvicine <caiopbonvicine@gmail.com>
2026-09-01 18:53:04 -0300

    fix: update test case to display error message for non-founded city search, now the test works and will pass

--- cypress/e2e/weather-now-tests/TC-006.cy.js
+++ cypress/e2e/weather-now-tests/TC-006.cy.js
@@ -3,16 +3,16 @@ describe('TC-006: Search for a city that has not been founded yet', () => {
     cy.visit('/');
   });
 
-  it('Should search for the entered city and display it if it has already been founded', () => {
+  it('Should display an error message when the city is entered', () => {
     cy.intercept('GET', '**/weather*').as('getWeather');
 
     cy.get('input[type="text"]').clear().type('Chris é Guloso por Brownies no matagal');
     cy.contains('button', 'Search').click();
 
     cy.wait('@getWeather').then((interception) => {
-      expect(interception.response.statusCode).to.eq(200);
-      const cityName = interception.response.body.city;
-      cy.log(`Captured city name: ${cityName}`);
+      expect(interception.response.statusCode).to.eq(404);
     });
+
+    cy.contains('City not found').should('be.visible');
   });
 });
