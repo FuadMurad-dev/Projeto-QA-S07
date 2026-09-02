ec002a38d047fdb7bdbced98192b056b7e6b6172
Fuad Murad <murad.fuad24@gmail.com>
2026-09-02 14:27:44 -0300

    feat: add initial test case to verify page loads correctly

--- cypress/e2e/weather-now-tests/TC011.cy.js
+++ cypress/e2e/weather-now-tests/TC011.cy.js
@@ -1,14 +1,17 @@
 
 
-describe("Tests" , () => {
+describe("TC-011: Verify that the page loads correctly" , () => {
     beforeEach(() => {
 
         cy.visit("http://localhost:3000/")
     })
 
-    it("Verify that the page loads correctly", () => {
+    it("Should display the correct title", () => {
         cy.get("h1").should("contain", "Weather Now")
         cy.get("input").should("be.visible")
         cy.get("button").should("be.visible")
     })
 })
+
+
+
